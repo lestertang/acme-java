ecd1c0af0eb5868b670398d0d47a8c5ad026c16e
skinzer <sean.kinzer@gmail.com>
2016-01-26 10:41:00 -0800

    fixed

--- src/main/java/ChrisMethodsTest.java
+++ src/main/java/ChrisMethodsTest.java
@@ -13,14 +13,24 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.richfaces.resource.MediaOutputResource;
 import org.springframework.http.converter.xml.AbstractXmlHttpMessageConverter;
 import org.springframework.http.converter.xml.SourceHttpMessageConverter;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+//import io.netty.handler.codec.http.websocketx.WebSocket08FrameDecoder
 
 import javax.faces.context.FacesContext;
 import java.io.IOException;
 
 public class ChrisMethodsTest {
+   
   public static void main(String[] args) throws Exception {
     ChrisMethodsTest chrisMethodsTest = new ChrisMethodsTest(args[0]);
-
+    
     System.out.println("matches: " + chrisMethodsTest.execute(args[1]));
   }
 
@@ -45,7 +55,7 @@ public class ChrisMethodsTest {
     BCrypt.checkpw(password, hashed);
 
     runSomeHttpTests(null);
-
+    compressDecompressSomeStuff(128);
     return false;
   }
 
@@ -62,5 +72,32 @@ public class ChrisMethodsTest {
 
     AbstractVerifier av = null;
     av.getCNs(null);
+  }
+  private void compressDecompressSomeStuff(Integer test) throws FileNotFoundException, IOException {
+        
+        FileInputStream fin = new FileInputStream("archive.tar.bz2");
+        BufferedInputStream in = new BufferedInputStream(fin);
+        FileOutputStream out = new FileOutputStream("archive.tar");
+        BZip2CompressorInputStream bzIn = new BZip2CompressorInputStream(in);
+        final byte[] buffer = new byte[test];
+        int n = 0;
+        while (-1 != (n = bzIn.read(buffer))) {
+          out.write(buffer, 0, n);
+       }
+        out.close();
+        bzIn.close();
+       BZip2CompressorOutputStream outputBZStream = null;
+       
+        try {
+            outputBZStream = new BZip2CompressorOutputStream(out);
+            
+            outputBZStream.finish();
+        } catch (Exception e) {
+            
+        }
+        
+       
+        
+        
   }
 }
